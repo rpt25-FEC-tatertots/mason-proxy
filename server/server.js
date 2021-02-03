edf4cdd097e2737ea364addd97a927a91c2b39f2
Mason Pott <masoncpott@gmail.com>
2021-02-03 01:04:14 -0600

    got things rendered and displaying off the product number in the url

--- server/server.js
+++ server/server.js
@@ -3,13 +3,24 @@ const axios = require('axios');
 
 const app = express();
 
-app.use(express.static('./public/dist'));
+app.use('/:product_id' ,express.static('./public/dist'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get('/photos', (req, res) => {
-  const { product_id } = req.query;
-  axios.get(`http://localhost:5001/photos?product_id=${product_id}`)
+app.get('/title/:product_id', (req, res) => {
+  const { product_id } = req.params;
+  axios.get(`http://localhost:5003/title/${product_id}`)
+  .then((response) => {
+    res.send(response.data)
+  })
+  .catch((error) => {
+    console.log('ERROR IN PROXY SERVER: ', error)
+  })
+})
+
+app.get('/photos/:product_id', (req, res) => {
+  const { product_id } = req.params;
+  axios.get(`http://localhost:5001/photos/${product_id}`)
   .then((response) => {
     res.send(response.data)
   })
@@ -20,8 +31,20 @@ app.get('/photos', (req, res) => {
 
 app.get('/overview', (req, res) => {
   // console.log('REQUEST: ', req.query.product_id)
-  const { product_id } = req.query;
-  axios.get(`http://localhost:5007/overview?product_id=${product_id}`)
+  const { product_id } = req.params;
+  axios.get(`http://localhost:5007/overview`)
+    .then((response) => {
+      res.send(response.data)
+    })
+    .catch((error) => {
+      console.log('ERROR IN PROXY SERVER: ', error)
+    })
+})
+
+app.get('/overview/:product_id', (req, res) => {
+  // console.log('REQUEST: ', req.query.product_id)
+  const { product_id } = req.params;
+  axios.get(`http://localhost:5007/overview/${product_id}`)
     .then((response) => {
       res.send(response.data)
     })
@@ -31,8 +54,8 @@ app.get('/overview', (req, res) => {
 })
 
 app.get('/similar', (req, res) => {
-  const { product_id } = req.query;
-  axios.get(`http://localhost:5008/similar?product_id=${product_id}`)
+  const { product_id } = req.params;
+  axios.get(`http://localhost:5008/similar/${product_id}`)
     .then((response) => {
       res.send(response.data)
     })
@@ -41,17 +64,17 @@ app.get('/similar', (req, res) => {
     })
 })
 
-app.get('/api/locations', (req, res) => {
-  const { product_id } = req.query;
-  axios.get(`http://localhost:5002/api/locations?product_id=${product_id}`)
-    .then((response) => {
-      console.log('RESPONSE FROM LOCATIONS: ', response.data)
-      res.send(response.data)
-    })
-    .catch((error) => {
-      console.log('ERROR IN PROXY SERVER: ', error)
-    })
-})
+// app.get('/api/locations', (req, res) => {
+//   const { product_id } = req.query;
+//   axios.get(`http://localhost:5002/api/locations?product_id=${product_id}`)
+//     .then((response) => {
+//       console.log('RESPONSE FROM LOCATIONS: ', response.data)
+//       res.send(response.data)
+//     })
+//     .catch((error) => {
+//       console.log('ERROR IN PROXY SERVER: ', error)
+//     })
+// })
 
 app.listen(5000, () => {
   console.log('listening on port 5000!');
